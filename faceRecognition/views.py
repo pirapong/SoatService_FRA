1ca6c10ada25a808930be677e138ab0b5627d5bc
pirapong.klaypan@gmail.com <pirapong.klaypan@gmail.com>
2024-07-24 10:50:06 +0000

    edit

--- faceRecognition/views.py
+++ faceRecognition/views.py
@@ -10,8 +10,8 @@ def fra(request):
     key = request.data['key']
     image = base64.b64decode(base64_string, validate=True)
     file_to_save = "img/" + key + ".jpg"
-    with open(file_to_save, "wb") as f:
-        f.write(image)
+    #with open(file_to_save, "wb") as f:
+    #    f.write(image)
     imgDb = face_recognition.load_image_file('imgDb/img.jpg')
     imgDb_encoding = face_recognition.face_encodings(imgDb)[0]
     imgDv = face_recognition.load_image_file(file_to_save)
@@ -20,7 +20,7 @@ def fra(request):
     for x in range(1, 100):
         results = face_recognition.compare_faces([imgDv_encoding], imgDb_encoding,x/100)
         if results[0] == True:
-            # print("เปอร์เซนต์ความถูกต้อง %d" % ((x-100) * (-1)))
+            print("เปอร์เซนต์ความถูกต้อง %d" % ((x-100) * (-1)))
             avg = "Accuracy %d " % ((x-100) * (-1)) + "%"
             break
 
