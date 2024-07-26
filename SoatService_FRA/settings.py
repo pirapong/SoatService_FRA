fd7b65223afc8bc2c81226b47bf945089a4b1a23
pirapong.klaypan@gmail.com <pirapong.klaypan@gmail.com>
2024-07-26 10:50:24 +0000

    edit

--- SoatService_FRA/settings.py
+++ SoatService_FRA/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
-# import os
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -138,3 +138,4 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
